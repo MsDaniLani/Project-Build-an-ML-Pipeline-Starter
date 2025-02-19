2ca86a864db75dd4e0f28519809a15514b03601d
Danielle Lamke <47258069+MsDaniLani@users.noreply.github.com>
2025-02-19 15:14:46 -0600

    Adding boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,10 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+
+    # Adding boundaries for the latitude and longitude
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     
     # Save the cleaned file
     df.to_csv('clean_sample.csv',index=False)
